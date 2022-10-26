307b0d76df8f4c8c4d9d9f6fd219effa29af33fb
MateusGomes04 <mateusgomesc04@gmail.com>
2022-10-26 09:24:25 -0300

    add Express session with Redis

--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,10 @@ res.render("admin/index");
 
 router.get("/login", function(req, res, next){
 
+    if (!req.session.views) req.session.views = 0;
+
+    console.log("SESSION:", req.session.views++);
+
     res.render("admin/login");
 
     });
